30317605c06a46147d11792b1c770eea1e07fc90
Timothy Cope <kirbycope@gmail.com>
2024-09-17 00:48:55 -0700

    jump

--- scenes/super-mario-bros/main.gd
+++ scenes/super-mario-bros/main.gd
@@ -28,7 +28,7 @@ func _ready() -> void:
 	if Globals.debug_mode: print(Globals.time_stamp, " [DEBUG] '", get_script().resource_path.get_file().get_basename(), "' scene loaded.")
 
 	# Disable the mouse pointer and capture the motion
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 	# Make sure the game is unpaused
 	Globals.game_paused = false
